dc6e27d092efa0beb48da9be5ac3b804b72c5c88
Eduardo Alvarez <ealvarez0896@gmail.com>
2019-08-21 16:43:18 -0600

    Update
    A message when the game is over has been added.
    The constructor of Snake class has been modified just for testing purposes.
    The Update method of Game class has been modified to close the window once the snake is no longer alive. This is achieved by passing the running variable by reference to the method and change the value to false.

---
+++ build/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/SnakeGame/src/controller.cpp" "/home/user/SnakeGame/build/CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
+  "/home/user/SnakeGame/src/game.cpp" "/home/user/SnakeGame/build/CMakeFiles/SnakeGame.dir/src/game.cpp.o"
+  "/home/user/SnakeGame/src/main.cpp" "/home/user/SnakeGame/build/CMakeFiles/SnakeGame.dir/src/main.cpp.o"
+  "/home/user/SnakeGame/src/renderer.cpp" "/home/user/SnakeGame/build/CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
+  "/home/user/SnakeGame/src/snake.cpp" "/home/user/SnakeGame/build/CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
